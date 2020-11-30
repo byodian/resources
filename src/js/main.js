76711ac548ebd26db4b6432348f63ccd4f08b596
byodian <byj6696@gmail.com>
2020-11-30 21:53:53 +0800

    Code refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,39 @@ const render = (function() {
     },
   };
 
+  // Generate a item of the navigation
+  const generateNavItem = category => `
+    <li class="left_menu_item">
+      <img class="menu_item_icon" src="./svg/example.svg"></img>
+      <span class="menu_item_content">${category}</span>
+    </li>
+  `;
+
+  // Generate a section of the main content
+  const generateSection = category => `
+    <section id="${category}" class="group" >
+      <h3 class="group_title">${category}</h3>
+      <div class="group_content">
+        <ul class="row group_items"></ul>
+      </div>
+    </section>  
+  `;
+
+  // Generate a list of the section 
+  const generateListOfSection = resource => `
+    <li class="group_item col3">
+      <a class="group_item_link" href="${resource.linkHref}">
+        <div class="card">
+          <img class="card_icon" src="${resource.src}">
+          <div class="card_body">
+            <h4 class="card_title">${resource.title}</h4>
+            <p class="card_text">${resource.content}</p>
+          </div>
+        </div>
+      </a>
+    </li>
+  `;
+
   // Methods
   // Remove duplicate values from an array
   const uniqueArray = function (arr) {
@@ -24,87 +57,52 @@ const render = (function() {
 
   // Insert left navigation items into document
   const renderNavItems = function (selector, categories) {
-    if(!document.querySelector) return;
-    const items = categories
-      .map((category) => {
-        return `
-          <li class="left_menu_item">
-            <img class="menu_item_icon" src="./svg/example.svg"></img>
-            <span class="menu_item_content">${category}</span>
-          </li>
-        `;
-      })
-      .join('')
+    // Check the selector  for validity
+    if(!document.querySelector(selector)) return;
 
+    const items = categories
+      .map(category => generateNavItem(category))
+      .join('')
     document.querySelector(selector).innerHTML = items;
   };
 
   // Render section groups to document
   const renderSectionGroups = function(selector, categories) {
+    // Check the selector for vaidity
     if (!document.querySelector(selector)) return;
     if (typeof categories !== 'object') return;
+    
     // Get section items
-    const groups = categories.map((category) => {
-      return `
-        <section id="${category}" class="group" >
-          <h3 class="group_title">${category}</h3>
-          <div class="group_content">
-            <ul class="row group_items"></ul>
-          </div>
-        </section>  
-      `;
-    });
-
-    document.querySelector(selector).innerHTML = groups.join('');
+    const groups = categories
+      .map(category => generateSection(category))
+      .join('');
+    document.querySelector(selector).innerHTML = groups;
   }
 
   // Get section items
   const renderSectionItmes = function(id, resources) {
     if (typeof resources !== 'object') return;
     if (!document.querySelector(`#${id} .group_items`)) return;
+
     const makeup = resources
       .filter(resource => resource.category.trim() === id)
-      .map(resource => {
-        return `
-          <li class="group_item col3">
-            <a class="group_item_link" href="${resource.linkHref}">
-              <div class="card">
-                <img class="card_icon" src="${resource.src}">
-                <div class="card_body">
-                  <h4 class="card_title">${resource.title}</h4>
-                  <p class="card_text">${resource.content}</p>
-                </div>
-              </div>
-            </a>
-          </li>
-        `;
-      })
+      .map(resource => generateListOfSection(resource))
       .join('')
 
     document.querySelector(`#${id} .group_items`).innerHTML = makeup;
   }
 
-  // Update the content 
-  var update = function(data) {
-    defaults.resources = defaults.resources.concat(data);
-    init(defaults.resources);
-  }
+  //
 
   // Init events
-  var init = function (options) {
+  const init = function (options) {
     options = options || {};
 
     // Merge both user defaults and options.
     const settings = Object.assign({}, defaults, options);
 
-    if (typeof settings.resources !== 'object') {
-      console.log(defaults.resources);
-      return;
-    };
     // Get all categories of resources
-    const categories = uniqueArray(settings.resources.map(
-      (resource) => resource.category
-    ));
+    const categories = uniqueArray(settings.resources.map(resource => resource.category));
 
     renderNavItems(settings.navItemContainer, categories);
     renderSectionGroups(settings.sectionsContainer, categories);
@@ -118,7 +116,13 @@ const render = (function() {
     document.querySelector('.top_bar_btn').addEventListener('click', toggleNav, false);
   };
 
-  // Get resources from 
+    // Update the content 
+  const update = function(data) {
+    defaults.resources = defaults.resources.concat(data);
+    init(defaults.resources);
+  }
+
+  // Get resources from the service side
   resourcesService
     .getAll()
     .then(data => {
@@ -127,7 +131,7 @@ const render = (function() {
 
   // resourcesService
   //   .create({
-  //     "title": "社区",
+  //     "title": "Happy Birthday",
   //     "content": "聚集地",
   //     "src": "./img/file-app.png",
   //     "linkHref": "https://eleduck.com/",
@@ -137,12 +141,8 @@ const render = (function() {
   //     update(data)
   //   })
 
-
 // Inits & Events
   that.init = init;
 
   return that;
-
-  // TODO
-  // Create the destory methods of plugin
 })();
